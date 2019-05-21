152aa0aaf2ef0e4d43a1962a03dec521b7dbd852
Mike Blakley <m.blakley@icloud.com>
2019-05-20 23:08:05 -0400

    deleteItems functional

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-console.log("testing the save");
+//1. use 'this'
+//2. dynamically add and remove HTML elements and apply styles
+//3. enter items  by adding text and hitting 'return' or 'add item' button
+//4. check and uncheck items by clicking the 'check' button
+//5. permanently remove items fro the list
+// try using .submit(), .preventDefault(), toggleClass(), and closest
+
+
+//---------code for adding new items---------->
+// $(function() {
+//     $('#js-shoppping-list-form').on("submit" "keydown===13", event => {
+//         event.preventDefault();
+//         let newItem = $('#shopping-list-entry').val();
+//         console.
+//     })
+    //$('ul').append(
+      //  <p>test</p>
+        //<div></div>
+        // <li>
+        // <span class="shopping-item">apples</span>
+        // <div class="shopping-item-controls">
+        //   <button class="shopping-item-toggle">
+        //     <span class="button-label">check</span>
+        //   </button>
+        //   <button class="shopping-item-delete">
+        //     <span class="button-label">delete</span>
+        //   </button>
+        // </div>
+        // </li>
+//     );
+// })
+
+
+
+
+//---------code to check/uncheck items---------->
+
+
+
+
+//---------code to delete items---------->
+$(function deleteItems() {
+    $('.shopping-item-delete').click(function(event) {
+        this.closest("li").remove();
+        console.log("delete was clicked");
+    });
+});
+
+
