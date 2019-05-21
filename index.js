d564207a80a4f1e6d8632207bfca65f610dd2963
Mike Blakley <m.blakley@icloud.com>
2019-05-21 09:54:45 -0400

    js updated to clear input field

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-//1. use 'this'
-//2. dynamically add and remove HTML elements and apply styles
-//3. enter items  by adding text and hitting 'return' or 'add item' button
-//4. check and uncheck items by clicking the 'check' button
-// try using .submit(), .preventDefault(), toggleClass()
-
 
 //---------code for adding new items---------->
 $(function addItems() {
     $('#js-shopping-list-form').focus().submit(function() {
         event.preventDefault();
         let newItem = $('#shopping-list-entry').val(); 
-        //need to clear the input field still
         $('ul').append(
             `<li>
             <span class="shopping-item">${newItem}</span>
@@ -24,27 +17,20 @@ $(function addItems() {
             </div>
             </li>`
         );   
+        //clear the input field
+        $('#shopping-list-entry').val('');
     });
 });
 
 
 
-//cod to check/uncheck items w/ event delegation
+//code to check/uncheck items w/ event delegation
 $(function toggleItems() {
     $('ul').on('click', '.shopping-item-toggle', function(event) {
         $(this).parent().siblings().toggleClass('shopping-item__checked');
     });
 });
 
-
-//---------code to check/uncheck items---------->
-// $(function toggleItems() {
-//     $('.shopping-item-toggle').click(function(event) {
-//         $(this).parent().siblings().toggleClass('shopping-item__checked');
-//         //let statusCheck = $(this).parent().siblings().is('.shopping-item__checked');
-//         //console.log(statusCheck);
-//     });
-// });
 
 
 //-------code to delete items w/ event delegation---------------->
